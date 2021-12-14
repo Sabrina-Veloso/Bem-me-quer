7818f11878ba8f71f87175d14503e66b85f6e97a
Sabrina <sabrinavelososilva@gmail.com>
2021-12-14 19:14:29 -0300

    deu certoooo

--- bem.js
+++ bem.js
@@ -2,15 +2,15 @@ var botao = document.getElementById("confirmacao");
 
 botao.addEventListener('click', () => {
     //pegar a pergunta
-    var pegarPergunta = document.getElementById("pergunta");
-    if (pegarPergunta == null) {
+    var pegarPergunta = document.getElementById("pergunta").value;
+    if (pegarPergunta == "") {
         alert("Faça a sua pergunta!");
     } else {
         console.log(pegarPergunta)
     }
     //pegando o número
-    var petalas = document.getElementById("numero");
-    if (petalas == null) {
+    var petalas = document.getElementById("numero").value;
+    if (petalas == "") {
         alert("Escolha um número!")
     } else {
         console.log(petalas)
