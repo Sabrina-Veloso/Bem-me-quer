e7f2abb5b5e5173299c7c492779297a96755e2a0
Sabrina <sabrinavelososilva@gmail.com>
2021-12-14 17:14:12 -0300

    o botão funciona

--- bem.js
+++ bem.js
@@ -1,21 +1,18 @@
 var botao = document.getElementById("confirmacao");
-var nada = null;
+
 botao.addEventListener('click', () => {
     //pegar a pergunta
-    const pegarPergunta = document.getElementById("pergunta");
-    if (pegarPergunta == nada) {
+    var pegarPergunta = document.getElementById("pergunta");
+    if (pegarPergunta == null) {
         alert("Faça a sua pergunta!");
     } else {
         console.log(pegarPergunta)
     }
     //pegando o número
-    const petalas = Number.document.getElementById("numero");
-    if (petalas == nada) {
+    var petalas = document.getElementById("numero");
+    if (petalas == null) {
         alert("Escolha um número!")
     } else {
         console.log(petalas)
     }
-
-
-
 })
